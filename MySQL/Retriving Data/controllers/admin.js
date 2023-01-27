938f434f4448aefed59b0bc0035d87e6a8aa2173
alka222 <alkabharadwaj222@gmail.com>
2023-01-27 15:33:01 +0530

    added delete final commit

--- MySQL/Retriving Data/controllers/admin.js
+++ MySQL/Retriving Data/controllers/admin.js
@@ -59,17 +59,23 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll(products => {
+  Product.fetchAll()
+  .then(([rows, fileData]) => {
     res.render('admin/products', {
-      prods: products,
+      prods: rows,
       pageTitle: 'Admin Products',
       path: '/admin/products'
     });
-  });
+  })
+  .catch(err => console.log(err));
+  
 };
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.deleteById(prodId);
-  res.redirect('/admin/products');
+  Product.deleteById(prodId)
+  .then(() =>{
+    res.redirect('/admin/products');
+  })
+  .catch(err=> console.log(err));
 };
